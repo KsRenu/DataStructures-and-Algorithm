9c315e936e9a604512b5aab7739bc4b992b47633
KsRenu <renuks0802@gmail.com>
2023-06-04 07:35:43 +0530

    swapping of two numbers

---
+++ Basic/src/numberSwapping.java
@@ -0,0 +1,13 @@
+public class numberSwapping {
+    public static void main(String[] args) {
+        int n1=5;
+        int n2=10;
+        int temp=0;
+        temp=n1;
+        n1=n2;
+        n2=temp;
+
+        System.out.println("n1: "+n1+" n2: "+n2);
+    }
+
+}
