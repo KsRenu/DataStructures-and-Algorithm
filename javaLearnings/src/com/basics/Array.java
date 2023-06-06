bfc1d5eeeb18275f5d4eded675ae24cd88f967e6
KsRenu <renuks0802@gmail.com>
2023-06-06 12:47:57 +0530

    Arrays

---
+++ javaLearnings/src/com/basics/Array.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int number[] = new int[5];
+        //int [] number = new int[5];
+        //int[] numbers = {1,2,3,4,45,5};
+        String [] names = {"name1","name2"};
+        System.out.println(names.length);
+        System.out.println(Arrays.toString(number));
+    }
+}
