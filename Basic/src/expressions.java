4211a9853057c8858da78cf84d7c4c04a2618b18
KsRenu <renuks0802@gmail.com>
2023-06-06 10:34:21 +0530

    updates

--- Basic/src/expressions.java
+++
@@ -1,6 +0,0 @@
-public class expressions {
-    public static void main(String[] args) {
-        int n1 =10;
-        System.out.println("n1+1(not stored in memeory as its not assigned) = "+ (n1+1) + "\nn1+1(memory in value incremented automaticaly as ++n1 is equal to n1+=1) = "+ (++n1)+ "\nn1+1(value in memory is changed and incremented , incremented value will be printed after assignment) = "+ (n1++) );
-    }
-}
--- GUI/src/dialogueBox.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class dialogueBox {
-    public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name: ");
-        JOptionPane.showMessageDialog(null,"Hello "+name);
-    }
-}
