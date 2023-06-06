f069cae33b81b6722da42b63936522ac3f3d6db3
KsRenu <renuks0802@gmail.com>
2023-06-06 10:35:01 +0530

    updates

---
+++ Old/Basic/src/Main.java
@@ -0,0 +1,11 @@
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello all");
+        System.out.print("This doesn't takes the next print statement to the next new line.");
+        System.out.print("Let's check");
+        System.out.println();
+        }
+    }
