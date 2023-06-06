888cbd296ce6971ed83f96b05b6166fe0289a7e2
KsRenu <renuks0802@gmail.com>
2023-06-06 11:49:25 +0530

    Math function

---
+++ javaLearnings/src/com/basics/Operators.java
@@ -0,0 +1,15 @@
+package com.basics;
+
+public class Operators {
+    public static void main(String[] args) {
+
+
+        //inbuilt math package
+        System.out.println(Math.max(10,90));
+        System.out.println(Math.min(24434,5434));
+        System.out.println(Math.abs(-342.42));
+        System.out.println((int)Math.pow(5,2));
+        System.out.println((int)Math.sqrt(25));
+        System.out.println(Math.PI);
+    }
+}
