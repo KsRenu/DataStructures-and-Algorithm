85c145e48a75de7332f546dc2ca8c98997ad4f36
KsRenu <renuks0802@gmail.com>
2023-06-07 12:42:12 +0530

    Inheritance

---
+++ javaLearnings/src/com/OOPS/Child.java
@@ -0,0 +1,12 @@
+package com.OOPS;
+
+public class Child extends Parent{
+    String c1;
+    String c2;
+    String c3;
+
+    Child(String p1, String p2) {
+        super(p1, p2);
+    }
+
+}
---
+++ javaLearnings/src/com/OOPS/Parent.java
@@ -0,0 +1,10 @@
+package com.OOPS;
+
+public class Parent {
+    String p1;
+    String p2;
+    Parent(String p1,String p2){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
