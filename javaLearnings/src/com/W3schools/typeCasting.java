83b06f03246274e4763d16d9e57de5cc1b13052a
Renu <20bcs015@mcet.in>
2023-06-14 20:22:58 +0530

    String Class

--- javaLearnings/src/com/W3schools/typeCasting.java
+++ javaLearnings/src/com/W3schools/typeCasting.java
@@ -3,7 +3,7 @@ package com.W3schools;
 public class typeCasting {
     public static void main(String[] args) {
         int num1 = 10;
-        double num2 = 10.0d;
+        double num2 = num1;
         int num3= (int)num2;
         System.out.println(num1);
         System.out.println(num2);
