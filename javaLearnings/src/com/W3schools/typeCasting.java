1f98559a259aaf0716d7f358e257b274a32a14e9
Renu <20bcs015@mcet.in>
2023-06-14 19:25:46 +0530

    Child class

---
+++ javaLearnings/src/com/W3schools/typeCasting.java
@@ -0,0 +1,13 @@
+package com.W3schools;
+
+public class typeCasting {
+    public static void main(String[] args) {
+        int num1 = 10;
+        double num2 = 10.0d;
+        int num3= (int)num2;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+    }
+}
