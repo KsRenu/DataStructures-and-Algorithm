2f24a7e3d9f5987e20cef23338749c1ab9f1f9e8
Renu <20bcs015@mcet.in>
2023-06-17 09:39:43 +0530

    3-D arrays

---
+++ GFG_Learnings/src/ThreeDimensionalArrays_learn.java
@@ -0,0 +1,19 @@
+public class ThreeDimensionalArrays_learn {
+    public static void main(String[] args) {
+
+
+        int[][][] arr = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
+
+        System.out.println("arr[0][0][0] = " + arr[0][0][0]);
+        System.out.println("arr[1][0][0] = " + arr[1][0][0]);
+        System.out.println("arr[0][1][0] = " + arr[0][1][0]);
+        System.out.println("arr[1][1][0] = " + arr[1][1][0]);
+        System.out.println("arr[0][0][1] = " + arr[0][0][1]);
+        System.out.println("arr[1][0][1] = " + arr[1][0][1]);
+        System.out.println("arr[0][1][1] = " + arr[0][1][1]);
+        System.out.println("arr[1][1][1] = " + arr[1][1][1]);
+
+
+
+    }
+}
