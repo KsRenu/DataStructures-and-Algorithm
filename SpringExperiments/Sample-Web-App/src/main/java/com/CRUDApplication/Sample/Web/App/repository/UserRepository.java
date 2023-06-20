e080107e8d1966dc560c9ab3ccd020abf54e3543
KsRenu <renuks0802@gmail.com>
2023-06-20 21:15:12 +0530

    Automatic Exam Hall Allocation

--- SpringExperiments/Sample-Web-App/src/main/java/com/CRUDApplication/Sample/Web/App/repository/UserRepository.java
+++ SpringExperiments/Sample-Web-App/src/main/java/com/CRUDApplication/Sample/Web/App/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.CRUDApplication.Sample.Web.App.repository;
 
-public interface UserRepository {
+import com.CRUDApplication.Sample.Web.App.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
---
+++ SpringExperiments/Sample-Web-App/src/main/java/com/CRUDApplication/Sample/Web/App/service/UserService.java
@@ -0,0 +1,7 @@
+package com.CRUDApplication.Sample.Web.App.service;
+
+import com.CRUDApplication.Sample.Web.App.entity.User;
+
+public interface UserService {
+    void addUser(User user);
+}
