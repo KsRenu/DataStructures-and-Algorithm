b69df50c62d1093a8cdab4534c9fb72d392c6b60
K.S.RENU <87896770+KsRenu@users.noreply.github.com>
2023-06-22 22:16:37 +0530

    organising files

--- SpringExperiments/Sample-Web-App/src/main/java/com/CRUDApplication/Sample/Web/App/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CRUDApplication.Sample.Web.App.repository;
-
-import com.CRUDApplication.Sample.Web.App.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
